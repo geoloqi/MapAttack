e4dbc206eb8c4e84ca2a25728583e69c43b7d966
Kyle Drake <kyledrake@gmail.com>
2011-03-06 01:30:16 -0800

    Add code to read in oauth_token

--- pdx_pacman.rb
+++ pdx_pacman.rb
@@ -5,13 +5,14 @@ Bundler.require
 
 class PdxPacman < Sinatra::Base
   
+  GEOLOQI_OAUTH_TOKEN = File.read 'oauth_token.txt'
+  
   get '/?' do
     erb :'index'
   end
   
   post '/trigger' do
     json_params = JSON.parse request.body
-    
   end
   
   post '/register' do
