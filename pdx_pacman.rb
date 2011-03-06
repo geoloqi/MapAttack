32321b7d872b13da16a37c1ff9eab0fd1e025c6b
Kyle Drake <kyledrake@gmail.com>
2011-03-06 02:25:49 -0800

    eat_dot to remove dot from geoloqi, add key directly to avoid gitignore issue (key will go away in a day), parse trigger

--- pdx_pacman.rb
+++ pdx_pacman.rb
@@ -5,17 +5,28 @@ Bundler.require
 
 class PdxPacman < Sinatra::Base
   
-  GEOLOQI_OAUTH_TOKEN = File.read 'oauth_token.txt'
+  GEOLOQI_OAUTH_TOKEN = 'ba1-138a8e75c1359c5d651120ca760ba8cce20b5f1d'
   
   get '/?' do
     erb :'index'
   end
   
   post '/trigger' do
-    json_params = JSON.parse request.body
+    json = JSON.parse request.body
+    eat_dot json['place']['place_id']
+    ''
   end
   
   post '/register' do
     
+  end
+  
+  private
+  
+  def eat_dot(place_id)
+    Typhoeus::Request.new "https://api.geoloqi.com/1/place/update/#{place_id}",
+                          :body          => {:extra => {:active => 0}}.to_json,
+                          :method        => :post,
+                          :headers       => {'Authorization' => "OAuth #{GEOLOQI_OAUTH_TOKEN}", 'Content-Type' => 'application/json'}
   end
 end
