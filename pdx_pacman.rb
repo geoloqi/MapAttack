d26e72dd8f50e60c1d5a92b79c422e205a333cc7
Aaron Parecki <aaron@parecki.com>
2011-03-25 15:35:14 -0700

    Return all the information in one ajax call: marker positions, player scores and positions

--- pdx_pacman.rb
+++ pdx_pacman.rb
@@ -1,4 +1,3 @@
-# ULTRA STEP: MAKE MULTIPLE LAYER SUPPORT = MULTIPLE GAME BOARDS
 
 class PdxPacman < Sinatra::Base
 
@@ -34,7 +33,7 @@ class PdxPacman < Sinatra::Base
     if @player == nil
     	@player = Player.new
 	    @player.profile_image = user_profile.profile_image
-	    @player.name = user_profile.name
+	    @player.name = user_profile.username
 	    @player.geoloqi_user_id = user_profile.user_id
 	    @player.token = shared_token.token
 	    @player.game = @game
@@ -59,30 +58,53 @@ class PdxPacman < Sinatra::Base
     @player = Player.first :game => Game.first(:layer_id => body.layer.layer_id), :geoloqi_user_id => body.user.user_id
     
     if body.place.extra.active.to_i == 1
-      Geoloqi.post Geoloqi::OAUTH_TOKEN, "place/update/#{body.place.place_id}", {:extra => {:active => 0}}
+      Geoloqi.post Geoloqi::OAUTH_TOKEN, "place/update/#{body.place.place_id}", {:extra => {:active => 0, :team => @player.team.name}}
       @player.add_points body.place.extra.points if body.place.extra && body.place.extra.points
       @player.send_message "You ate a dot! #{body.place.extra.points} points"
     end
   end
 
-  get '/scores.json' do
-    content_type 'application/json'
-    players = Player.all.collect{|player| {:geoloqi_id => player.id,
-                                           :score => player.points_cache,
-                                           :name => player.name,
-                                           :profile_image => player.profile_image}}
-    players.to_json
-  end
+  get '/game/:layer_id/status.json' do
+    # content_type 'application/json'
 
-  get '/game/:layer_id/setup.json' do
     response = Geoloqi.post Geoloqi::OAUTH_TOKEN, 'place/list', {:layer_id => params[:layer_id]}
+
+	@game = Game.first :layer_id => params[:layer_id]
+
     places = []
     response['places'].each do |place|
       places << {:place_id => place['place_id'],
                  :latitude => place['latitude'],
                  :longitude => place['longitude'],
+                 :points => place['extra']['points'],
+                 :team => place['extra']['team'],
                  :active => place['extra']['active']}
     end
-    places.to_json
+    
+    @tokens = []
+    @game.player.each do |player|
+      @tokens.push player.token
+    end
+    response = Geoloqi.get Geoloqi::OAUTH_TOKEN, 'share/last?geoloqi_token=,' + @tokens.join(",")
+
+    players = []
+    @game.player.each do |player|
+    	location = {}
+    	response.each do |p|
+    	  puts p
+    	  if p['username'] == player.name
+    	    location = p
+    	  end
+    	end
+    
+    	players << {:geoloqi_id => player.id,
+	                   :score => player.points_cache,
+	                   :name => player.name,
+	                   :profile_image => player.profile_image,
+	                   :location => location}
+    end
+
+
+    {:places => places, :players => players}.to_json
   end
 end
