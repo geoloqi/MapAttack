aa9e6269bec0841fab09b1dfbbe4f4841cd82de0
Kyle Drake <kyledrake@gmail.com>
2011-03-24 20:14:50 -0700

    Fix get call for api

--- models/geoloqi.rb
+++ models/geoloqi.rb
@@ -2,14 +2,20 @@ module Geoloqi
   API_URL = 'https://api.geoloqi.com/1/'
   def self.headers(oauth_token); {'Authorization' => "OAuth #{oauth_token}", 'Content-Type' => 'application/json'} end
   def self.post(oauth_token, url, body)
+    
+    respone = 
+    
     SymbolTable.new JSON.parse(Typhoeus::Request.run(API_URL + url,
                                                      :method  => :post,
                                                      :body    => body.to_json,
-                                                     :headers => Geoloqi.headers(oauth_token))).body
+                                                     :headers => Geoloqi.headers(oauth_token))).response.body
   end
   def self.get(oauth_token, url)
-    SymbolTable.new JSON.parse(Typhoeus::Request.run(API_URL+url,
-                                                     :method  => :get,
-                                                     :headers => Geoloqi.headers(oauth_token))).body
+    
+    response = Typhoeus::Request.run(API_URL+url,
+                                     :method  => :get,
+                                     :headers => Geoloqi.headers(oauth_token))
+    puts response.body.inspect
+    SymbolTable.new JSON.parse(response.body)
   end
 end
