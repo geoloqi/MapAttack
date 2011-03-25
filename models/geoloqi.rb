53681562b56a5ec07ab02c8c7f328dd290d3e787
Kyle Drake <kyledrake@gmail.com>
2011-03-24 20:22:07 -0700

    Clean up and get api methods working

--- models/geoloqi.rb
+++ models/geoloqi.rb
@@ -2,19 +2,17 @@ module Geoloqi
   API_URL = 'https://api.geoloqi.com/1/'
   def self.headers(oauth_token); {'Authorization' => "OAuth #{oauth_token}", 'Content-Type' => 'application/json'} end
   def self.post(oauth_token, url, body)
-    
-    # response = 
-    
-    SymbolTable.new JSON.parse(Typhoeus::Request.run(API_URL + url,
-                                                     :method  => :post,
-                                                     :body    => body.to_json,
-                                                     :headers => Geoloqi.headers(oauth_token))).response.body
+    response = Typhoeus::Request.run API_URL+url,
+                                     :method  => :post,
+                                     :body => body.to_json,
+                                     :headers => Geoloqi.headers(oauth_token)
+    puts response.body.inspect
+    SymbolTable.new JSON.parse(response.body)
   end
   def self.get(oauth_token, url)
-    
-    response = Typhoeus::Request.run(API_URL+url,
+    response = Typhoeus::Request.run API_URL+url,
                                      :method  => :get,
-                                     :headers => Geoloqi.headers(oauth_token))
+                                     :headers => Geoloqi.headers(oauth_token)
     puts response.body.inspect
     SymbolTable.new JSON.parse(response.body)
   end
