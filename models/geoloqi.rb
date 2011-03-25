df9a46566a616bd70798ff6fb931267a2aa58486
Kyle Drake <kyledrake@gmail.com>
2011-03-24 20:15:23 -0700

    Dangling variable

--- models/geoloqi.rb
+++ models/geoloqi.rb
@@ -3,7 +3,7 @@ module Geoloqi
   def self.headers(oauth_token); {'Authorization' => "OAuth #{oauth_token}", 'Content-Type' => 'application/json'} end
   def self.post(oauth_token, url, body)
     
-    respone = 
+    # response = 
     
     SymbolTable.new JSON.parse(Typhoeus::Request.run(API_URL + url,
                                                      :method  => :post,
