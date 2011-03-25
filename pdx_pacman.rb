cc615da1db7b023f81423582482a7b7085f9ff92
Aaron Parecki <aaron@parecki.com>
2011-03-25 16:54:15 -0700

    Really fixed the join screen

--- pdx_pacman.rb
+++ pdx_pacman.rb
@@ -13,7 +13,7 @@ class PdxPacman < Sinatra::Base
     
     response = Geoloqi.get @oauth_token, 'layer/info/' + params[:layer_id]
     
-    if response.subscription.nil? || response.subscription == false
+    if response.subscription.nil? || response.subscription == false || response.subscription.subscribed.to_i == 0
 	    erb :join
 	else
     	redirect "/game/" + params[:layer_id] + "/mobile"
